080e9f888ece63b893c2815f4e1deaa18fdb3522
a.i.zueva <a.i.zueva@tinkoff.ru>
2019-11-13 15:21:27 +0300

    add file reader

--- app/src/main/java/com/example/hwstory/StoryActivity.kt
+++ app/src/main/java/com/example/hwstory/StoryActivity.kt
@@ -3,16 +3,7 @@ package com.example.hwstory
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_story.*
-import java.util.*
-import android.content.res.AssetManager
-import android.app.Activity
-import androidx.core.app.ComponentActivity
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-import sun.jvm.hotspot.utilities.IntArray
-
-
+import java.io.BufferedReader
 
 class StoryActivity : AppCompatActivity() {
 
@@ -24,23 +15,27 @@ class StoryActivity : AppCompatActivity() {
 
     private fun getStory(): String {
         return when (intent.getStringExtra("storyType")) {
-            "Скучная" -> readFile("boring.txt")
-            "Норм" -> readFile("norm.txt")
-            "Ну такое себе" -> readFile("ny.txt")
+            "Скучная" -> preparingText("files/boring.txt")
+            "Норм" -> preparingText("files/norm.txt")
+            "Ну такое себе" -> preparingText("files/ny.txt")
             else -> "Нет истории"
         }
     }
 
-    private fun readFile(filePath: String): String {
-        var text = ""
-        val file = Scanner(filePath)
-        while (file.hasNext()) {
-            text = "$text ${file.next()}"
-        }
+    private fun preparingText(filePath: String): String {
+        val text = readFileFromAssets(filePath)
         val words = intent.getShortArrayExtra("words")
         for (word in words) {
             text.format(word)
         }
+        return text
+    }
+
+    private fun readFileFromAssets(path: String): String {
+        var am = this.assets
+        var stream = am.open(path)
+        val text = stream.bufferedReader().use(BufferedReader::readText)
+        stream.close()
         return text
     }
 }
